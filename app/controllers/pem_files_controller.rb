b3b0cc037234665243009d953b97922c79fd36e6
Geovany Gameros <geovanygameros@gmail.com>
2019-01-15 15:42:08 -0700

    Se agrega css a login y funcionamiento terminado

--- app/controllers/pem_files_controller.rb
+++ app/controllers/pem_files_controller.rb
@@ -1,10 +1,11 @@
 class PemFilesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_pem_file, only: [:show, :edit, :update, :destroy]
 
   # GET /pem_files
   # GET /pem_files.json
   def index
-    @pem_files = PemFile.all
+    @pem_file = PemFile.where(email: session[:user_email]).where(status: PemFile::ACTIVE).last
   end
 
   # GET /pem_files/1
@@ -24,17 +25,29 @@ class PemFilesController < ApplicationController
   # POST /pem_files
   # POST /pem_files.json
   def create
-    @pem_file = PemFile.new(pem_file_params)
-
-    respond_to do |format|
+    require 'openssl'
+    password = params[:password]
+    prefix_file_name = session[:user_email].split('@').first
+    key_temp = Rails.root.join(pem_file_params[:key].tempfile)
+    cer_temp = Rails.root.join(pem_file_params[:cer].tempfile)
+    key_success = system "openssl pkcs8 -inform DER -in #{key_temp} -out public/#{prefix_file_name}.key.pem -passin pass:#{password}"
+    key_success = system "openssl rsa -in public/#{prefix_file_name}.key.pem -out public/#{prefix_file_name}.key.pem -aes256 -passout pass:#{password}"
+    cer_success = system "openssl x509 -inform DER -outform PEM -in #{cer_temp} -pubkey > public/#{prefix_file_name}.cer.pem"
+    if key_success && cer_success
+      @pem_file = PemFile.new(
+      key:File.read("public/#{prefix_file_name}.key.pem"),
+      cer:File.read("public/#{prefix_file_name}.cer.pem"),
+      email: session[:user_email]
+      )
       if @pem_file.save
-        format.html { redirect_to @pem_file, notice: 'Pem file was successfully created.' }
-        format.json { render :show, status: :created, location: @pem_file }
+        redirect_to root_path, notice: 'PEM creado exitósamente'
       else
-        format.html { render :new }
-        format.json { render json: @pem_file.errors, status: :unprocessable_entity }
+        redirect_to root_path, alert: 'No fue posible guardar el PEM'
       end
+    else
+      redirect_to root_path, alert: 'No fue posible crear el PEM'
     end
+    system "rm #{Rails.root.join("public","#{prefix_file_name}.*")}"
   end
 
   # PATCH/PUT /pem_files/1
@@ -42,11 +55,11 @@ class PemFilesController < ApplicationController
   def update
     respond_to do |format|
       if @pem_file.update(pem_file_params)
-        format.html { redirect_to @pem_file, notice: 'Pem file was successfully updated.' }
-        format.json { render :show, status: :ok, location: @pem_file }
+        format.html {redirect_to @pem_file, notice: 'Se actualizó el PEM'}
+        format.json {render :show, status: :ok, location: @pem_file}
       else
-        format.html { render :edit }
-        format.json { render json: @pem_file.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @pem_file.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -54,21 +67,23 @@ class PemFilesController < ApplicationController
   # DELETE /pem_files/1
   # DELETE /pem_files/1.json
   def destroy
-    @pem_file.destroy
+    @pem_file.update(status: PemFile::DELETED)
     respond_to do |format|
-      format.html { redirect_to pem_files_url, notice: 'Pem file was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to pem_files_url, notice: 'Se eliminaron los archivos PEM'}
+      format.json {head :no_content}
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pem_file
-      @pem_file = PemFile.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def pem_file_params
-      params.require(:pem_file).permit(:email, :status, :key, :cer)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_pem_file
+    @pem_file = PemFile.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def pem_file_params
+    params.require(:pem_file).permit(:email, :status, :key, :cer)
+  end
+
 end
